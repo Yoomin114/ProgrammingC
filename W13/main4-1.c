49f184a99c884725c233294b003ecc7bf3644478
Yoomin114 <goldchu@sookmyung.ac.kr>
2024-12-02 01:43:45 +0900

    w13

--- W13/main4-1.c
+++ W13/main4-1.c
@@ -5,4 +5,11 @@ struct point{
 	int		y;
 };
 
-int getArea(struct point p1, )
+int getArea(struct point p1, struct point p2);
+
+int main(int argc, char *argv[]){
+	
+	struct point p1, p2;
+	
+	area = getArea(p1, p2);
+}
